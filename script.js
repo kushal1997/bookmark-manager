e37b3cda822a6fe7bbd4df46b71cfb247ab1d97d
kushal1997 <kushalrao103@gmail.com>
2023-03-17 13:27:11 +0530

    accessed data from firebase

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.18.0/firebase-app.js";
-import { getFirestore, collection, addDoc, serverTimestamp } from "https://www.gstatic.com/firebasejs/9.18.0/firebase-firestore.js";
+import { getFirestore, collection, getDocs, addDoc, serverTimestamp } from "https://www.gstatic.com/firebasejs/9.18.0/firebase-firestore.js";
 
 const firebaseConfig = {
     apiKey: "AIzaSyD5It1IQXxZT57QNzF9_A1wphQbLY6L9Cw",
@@ -24,6 +24,36 @@ addForm.addEventListener("submit", event =>{
         category: addForm.category.value,
         createdAt: serverTimestamp()
     }).then(()=>{
+        showCard();
         addForm.reset();
     });
 });
+
+function generateTemplate(response,id){
+    return `<div class="card">
+                <p class="title">${response.title}</p>
+                <div class="subInformation">
+                    <p>
+                        <span class="category ${response.category}">${response.category[0].toUpperCase()}${response.category.slice(1)} </span>
+                    </p>
+                    <a href="${response.link}" target="_blank"><i class="bi bi-box-arrow-up-right website"></i></a>
+                    <a href="https://www.google.com/search?q=${response.title}" target="_blank"><i class="bi bi-google search"></i></a>
+                    <span><i class="bi bi-trash delete" data-id="${response.id}"></i></span>
+                </div>
+            </div>`;
+}
+
+const cards=document.querySelector(".cards");
+function showCard(){
+    cards.innerHTML="";
+    getDocs(colRef)
+        .then((data)=>{
+            data.docs.forEach(document =>{
+                cards.innerHTML+= generateTemplate(document.data(),document.id);
+            })
+        })
+        .catch((error)=>{
+            console.log(error);
+        })
+}
+showCard();
